94d6f989452ae59e5c0534cc30d199c145b99f9b
Sachintha Pathum Ravihansa <sachintha2024@gmail.com>
2024-08-27 12:54:40 +0530

    Get the resources to Start the project

---
+++ backEnd/abc_restaurant/abc_restaurant/src/main/java/com/sprrestaurant/AbcRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.sprrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcRestaurantApplication.class, args);
+	}
+
+}
